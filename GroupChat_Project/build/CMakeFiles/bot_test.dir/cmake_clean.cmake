4925e4b06451e94f791b5aec5c4cc096f80a081e
Luvell-G <trippmc16@yahoo.com>
2026-04-23 09:55:02 -0400

    Add files via upload

---
+++ GroupChat_Project/build/CMakeFiles/bot_test.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bot_test.dir/link.d"
+  "CMakeFiles/bot_test.dir/tests/bot_test.cpp.o"
+  "CMakeFiles/bot_test.dir/tests/bot_test.cpp.o.d"
+  "bot_test"
+  "bot_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bot_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
